4f2c1818acd19b703f43f230bc17dd0eaa5c9eb0
salah <salahswidan212@gmail.com>
2025-01-07 20:30:39 +0200

    make animation to custom splash screen

--- lib/core/resources/route_manager.dart
+++ lib/core/resources/route_manager.dart
@@ -1,11 +1,14 @@
+import 'package:drosak_managment_app/view/on_boarding/screens/on_boarding_screen.dart';
 import 'package:drosak_managment_app/view/splash/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class RouteManager {
   static Map<String, WidgetBuilder> routes = {
     RouteNames.kSplashScreen: (context) => const SplashScreen(),
+    RouteNames.kOnBoardingScreen: (context) => const OnBoardingScreen(),
   };
 }class RouteNames{
 
   static const String kSplashScreen = '/KSplashScreen';
+  static const String kOnBoardingScreen = '/kOnBoardingScreen';
 }
--- lib/view/splash/screens/splash_screen.dart
+++ lib/view/splash/screens/splash_screen.dart
@@ -1,25 +1,68 @@
 import 'package:drosak_managment_app/core/resources/colors_manager.dart';
+import 'package:drosak_managment_app/core/resources/route_manager.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _animationBottom;
+  late Animation<Offset> _animationTop;
+  @override
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addStatusListener((status) {
+        if(status == AnimationStatus.completed)
+        {
+          Navigator.of(context).pushReplacementNamed(RouteNames.kOnBoardingScreen);
+        }
+      });
+    _animationBottom = Tween<Offset>(begin: Offset(0, 1), end: Offset.zero)
+        .animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut));
+    _animationController.forward();
+    _animationTop = Tween<Offset>(begin: Offset(0, -1), end: Offset.zero)
+        .animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut));
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorsManager.kBlackColor,
-      body: Container(
+      body: SizedBox(
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset("assets/images/splash_border_image.png"),
+            SlideTransition(
+                position: _animationTop,
+                child: Image.asset("assets/images/splash_border_image.png")),
             Image.asset("assets/images/logo.png"),
             Align(
                 alignment: AlignmentDirectional.bottomEnd,
-                child: Image.asset(
-                    "assets/images/splash_border_image_bottom.png")),
+                child: SlideTransition(
+                  position: _animationBottom,
+                  child: Image.asset(
+                      "assets/images/splash_border_image_bottom.png"),
+                )),
           ],
         ),
       ),
