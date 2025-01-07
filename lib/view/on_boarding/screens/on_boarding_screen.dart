d0c7e9b5eaa15ba9c1614d07dc0b65d3874ae450
salah <salahswidan212@gmail.com>
2025-01-07 21:24:01 +0200

    add font aa galexy and add flutter svg package

--- lib/view/on_boarding/screens/on_boarding_screen.dart
+++ lib/view/on_boarding/screens/on_boarding_screen.dart
@@ -1,4 +1,8 @@
+import 'package:drosak_managment_app/core/resources/assets_values_manager.dart';
+import 'package:drosak_managment_app/core/resources/colors_manager.dart';
+import 'package:drosak_managment_app/core/resources/const_value.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -6,9 +10,31 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("OnBoarding Screen"),
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              AssetsValuesManager.kOnBoardingImage1,
+              height: 200,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              ConstValue.kOnBoardingText1,
+              style: TextStyle(
+                  fontFamily: "AA-GALAXY",
+                  fontSize: 20,
+                  color: ColorsManager.kBlackColor),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
