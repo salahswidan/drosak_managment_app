aa93f98dd13c249bc35bd8ddb14d73820f75c3a8
salah <salahswidan212@gmail.com>
2025-01-26 11:12:52 +0200

    make factory item stage model

--- lib/core/database/sqlflite/education_stage_operation.dart
+++ lib/core/database/sqlflite/education_stage_operation.dart
@@ -1,18 +1,24 @@
-
- import '../../../model/education_stage/item_stage_model.dart';
+import '../../../model/education_stage/item_stage_model.dart';
 import 'my_sqlflite_database.dart';
 
 class EducationStageOperation extends MySqlFliteDatabase {
   Future<bool> insertEducationDetails(ItemStageModel itemStageModel) {
-    return insert(tableName: MySqlFliteDatabase.educationalStageTableName, values: {
-     MySqlFliteDatabase.educationalStageName:itemStageModel.stageName,
-     MySqlFliteDatabase.educationalStageDesc:itemStageModel.desc,
-     MySqlFliteDatabase.educationalStageImage:itemStageModel.image,
-     
-    });
-  }
-  Future<List<Map<String, Object?>>> getAllEducationData(){
-    return select(tableName: MySqlFliteDatabase.educationalStageTableName);
+    return insert(
+        tableName: MySqlFliteDatabase.educationalStageTableName,
+        values: {
+          MySqlFliteDatabase.educationalStageName: itemStageModel.stageName,
+          MySqlFliteDatabase.educationalStageDesc: itemStageModel.desc,
+          MySqlFliteDatabase.educationalStageImage: itemStageModel.image,
+        });
   }
 
+  Future<List<ItemStageModel>> getAllEducationData() async {
+    List<ItemStageModel> listItemStageModel = [];
+    List<Map<String, Object?>> data =
+        await select(tableName: MySqlFliteDatabase.educationalStageTableName);
+    for (var item in data) {
+      listItemStageModel.add(ItemStageModel.fromJson(item));
+    }
+    return listItemStageModel;
   }
+}
--- lib/model/education_stage/item_stage_model.dart
+++ lib/model/education_stage/item_stage_model.dart
@@ -4,10 +4,23 @@ class ItemStageModel {
   final String stageName;
   final String desc;
 
+@override
+  String toString (){
+    return 'itemStageModel{id: $id, image: $image, stageName: $stageName, desc: $desc}';
+    }
+  
   ItemStageModel({
     required this.id,
     required this.image,
     required this.stageName,
     required this.desc,
   });
+  factory ItemStageModel.fromJson(Map json) {
+    return ItemStageModel(
+      id: int.parse(json['id'].toString()),
+      stageName: json['name'].toString(),
+      desc: json['desc'].toString(),
+      image: json['image'].toString(),
+    );
+  }
 }
