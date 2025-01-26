aa1975c708d72233637be7534b8b91c33564cdad
salah <salahswidan212@gmail.com>
2025-01-26 09:52:26 +0200

    add new stage name

--- lib/controller/education_stage/education_stage_controller.dart
+++ lib/controller/education_stage/education_stage_controller.dart
@@ -1,3 +1,5 @@
+import 'package:drosak_managment_app/core/database/sqlflite/education_stage_operation.dart';
+import 'package:drosak_managment_app/core/resources/assets_values_manager.dart';
 import 'package:drosak_managment_app/model/education_stage/item_stage_model.dart';
 import 'package:flutter/material.dart';
 import '../../view/education_stages/widget/custom_add_new_education_stage.dart';
@@ -10,11 +12,27 @@ class EducationStageController {
       TextEditingController();
   void openBottomSheet({required BuildContext context}) {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         builder: (context) => CustomAddNewEducationStage(
-            controllerDescraptinEducationalStage: controllerDescraptinEducationalStage,
+            onPressed: () {
+              addToNewEducation();
+            },
+            controllerDescraptinEducationalStage:
+                controllerDescraptinEducationalStage,
             controllerNameEducationalStage: controllerNameEducationalStage));
   }
 
-  void addToNewEducationalStageToDB() {}
+  void addToNewEducation() async {
+    EducationStageOperation educationStageOperation = EducationStageOperation();
+  bool inserted =await  educationStageOperation.insertEducationDetails(
+      ItemStageModel(
+        id: 0,
+        stageName: controllerNameEducationalStage.text,
+        desc: controllerDescraptinEducationalStage.text,
+        image: AssetsValuesManager.assetsImagesPlaceholderSvg,
+      ),
+    );
+    print(inserted);
+  }
 }
--- lib/view/education_stages/widget/custom_add_new_education_stage.dart
+++ lib/view/education_stages/widget/custom_add_new_education_stage.dart
@@ -11,62 +11,71 @@ import '../../../core/widget/input_field/custom_text_form_field.dart';
 class CustomAddNewEducationStage extends StatelessWidget {
   const CustomAddNewEducationStage({
     super.key,
+    required this.onPressed,
     required this.controllerNameEducationalStage,
     required this.controllerDescraptinEducationalStage,
   });
+    final VoidCallback onPressed;
 
   final TextEditingController controllerNameEducationalStage;
   final TextEditingController controllerDescraptinEducationalStage;
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 20.h),
-        decoration: BoxDecoration(
-          color: ColorsManager.kBlackLight,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(12.r),
-            topRight: Radius.circular(12.r),
-          ),
-        ),
-        width: double.infinity,
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Row(
-            children: [
-              Expanded(
-                child: CustomTextFormField(
-                  hintText: ConstValue.kNameEducationalStages,
-                  controller: controllerNameEducationalStage,
-                ),
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 20.h),
+            decoration: BoxDecoration(
+              color: ColorsManager.kBlackLight,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.r),
+                topRight: Radius.circular(12.r),
+              ),
+            ),
+            width: double.infinity,
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomTextFormField(
+                      hintText: ConstValue.kNameEducationalStages,
+                      controller: controllerNameEducationalStage,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 6.w,
+                  ),
+                  IconButton.filled(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorsManager.kPrimaryColor),
+                      icon:
+                          SvgPicture.asset(AssetsValuesManager.kPlaceholderSvg))
+                ],
               ),
               SizedBox(
-                width: 6.w,
+                height: 12.h,
               ),
-              IconButton.filled(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: ColorsManager.kPrimaryColor),
-                  icon: SvgPicture.asset(AssetsValuesManager.kPlaceholderSvg))
-            ],
+              CustomTextFormField(
+                maxLines: 3,
+                hintText: ConstValue.kDescEducationalStage,
+                controller: controllerDescraptinEducationalStage,
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
+              CustomMaterialButton(
+                onPressed: onPressed,
+                text: ConstValue.kAdd,
+              )
+            ]),
           ),
-          SizedBox(
-            height: 12.h,
-          ),
-          CustomTextFormField(
-            maxLines: 3,
-            hintText: ConstValue.kDescEducationalStage,
-            controller: controllerDescraptinEducationalStage,
-          ),
-          SizedBox(
-            height: 40.h,
-          ),
-          CustomMaterialButton(
-            onPressed: () {},
-            text: ConstValue.kAdd,
-          )
-        ]),
+        ),
       ),
     );
   }
