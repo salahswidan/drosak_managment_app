63a4945867bbade41efa8a0e4872886c44f91d55
salah <salahswidan212@gmail.com>
2025-02-04 10:56:45 +0200

    show alert dialog to select iamge from where

--- lib/controller/education_stage/education_stage_controller.dart
+++ lib/controller/education_stage/education_stage_controller.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 
 import 'package:drosak_managment_app/core/database/sqlflite/education_stage_operation.dart';
+import 'package:drosak_managment_app/core/resources/colors_manager.dart';
+import 'package:drosak_managment_app/core/resources/const_value.dart';
+import 'package:drosak_managment_app/core/resources/font_manager.dart';
 import 'package:drosak_managment_app/model/education_stage/item_stage_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../view/education_stages/widget/custom_add_new_education_stage.dart';
 
@@ -80,7 +84,7 @@ class EducationStageController {
               inputPathImage.add(pathImage);
             },
             onPressedPickImage: () {
-              pickImageFromGallary();
+              showCustomDialogChooseImage(context);
             },
             onPressedAdd: () async {
               if (formKey.currentState!.validate() == true) {
@@ -115,5 +119,66 @@ class EducationStageController {
       ),
     );
     return inserted;
+  }
+
+  void showCustomDialogChooseImage(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => AlertDialog(
+              title:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                IconButton.filled(
+                  iconSize: 50,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: ColorsManager.kPrimaryColor,
+                  ),
+                  onPressed: () {
+                    pickImageFromGallary();
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.image),
+                ),
+                SizedBox(
+                  width: 30.w,
+                ),
+                IconButton.filled(
+                  iconSize: 50,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: ColorsManager.kPrimaryColor,
+                  ),
+                  onPressed: () {
+                    pickImageFromCamera();
+                  },
+                  icon: Icon(Icons.camera_alt),
+                ),
+              ]),
+              icon: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton.filled(
+                    iconSize: 10,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.close,
+                      size: 20.0,
+                      color: Colors.white,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                  const Text(
+                    ConstValue.kChooseFrom,
+                    style: TextStyle(
+                        fontFamily: FontName.geDinerOne,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 15),
+                  ),
+                ],
+              ),
+            ));
   }
 }
