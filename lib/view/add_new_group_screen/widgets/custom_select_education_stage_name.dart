0b888f77138679bc08942a19fc44818e42f09da8
salah <salahswidan212@gmail.com>
2025-02-09 09:08:47 +0200

    make header builder to custom Dropdown of education stage

--- lib/view/add_new_group_screen/widgets/custom_select_education_stage_name.dart
+++ lib/view/add_new_group_screen/widgets/custom_select_education_stage_name.dart
@@ -1,6 +1,7 @@
 import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/resources/colors_manager.dart';
 import '../../../core/resources/const_value.dart';
 import '../../../core/resources/font_manager.dart';
 import '../../../model/education_stage/item_stage_model.dart';
@@ -53,18 +54,44 @@ class CustomSelectEducationStageNameAddNewGroup extends StatelessWidget {
                     contentPadding: EdgeInsets.zero,
                     title: Text(
                       item.stageName,
+                      style: TextStyle(color: Colors.black, fontSize: 14.sp),
                     ),
                     subtitle: item.desc.isEmpty
                         ? Text(
-                            "null",
+                            "",
                             style: TextStyle(color: Colors.black),
                           )
                         : Text(item.desc),
-                    leading: CircleAvatar(child: Text(item.id.toString())),
+                    leading: CircleAvatar(
+                        radius: 10.r,
+                        backgroundColor: ColorsManager.kPrimaryColor,
+                        child: Text(item.id.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10.sp,
+                            ))),
                   ),
-                  onChanged: (p0) {
-                    print(p0);
-                  },
+                  onChanged: onChange,
+                  itemsListPadding: EdgeInsets.zero,
+                  headerBuilder: (context, selectedItem, enabled) =>
+                      Row(children: [
+                    CircleAvatar(
+                        radius: 10.r,
+                        backgroundColor: ColorsManager.kPrimaryColor,
+                        child: Text(selectedItem.id.toString(),
+                            style: TextStyle(
+                              fontSize: 10.sp,
+                              color: Colors.white,
+                            ))),
+                    SizedBox(
+                      width: 15.w,
+                    ),
+                    Expanded(
+                        child: Text(
+                      selectedItem.stageName,
+                      style: TextStyle(color: Colors.black, fontSize: 14.sp),
+                    )),
+                  ]),
                 ),
         ),
         SizedBox(
