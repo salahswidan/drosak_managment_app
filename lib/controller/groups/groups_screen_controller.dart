d843f87443968d6b5f2c3e0fc4d0d1604c2bbab0
salah <salahswidan212@gmail.com>
2025-02-16 14:01:42 +0200

    finish group screen

--- lib/controller/groups/groups_screen_controller.dart
+++ lib/controller/groups/groups_screen_controller.dart
@@ -1,16 +1,11 @@
 import 'dart:async';
 import 'package:drosak_managment_app/core/resources/const_value.dart';
 import 'package:drosak_managment_app/core/widget/search/custom_search_delgate_education_stage_screen.dart';
-import 'package:drosak_managment_app/model/education_stage/item_stage_model.dart';
-import 'package:drosak_managment_app/model/group/appointment_model.dart';
-import 'package:drosak_managment_app/model/group/group_details.dart';
 import 'package:drosak_managment_app/model/group/group_info_model.dart';
 import 'package:drosak_managment_app/view/groups/widgets/custom_list_search_group_screen.dart';
 import 'package:flutter/material.dart';
-import '../../core/database/sqlflite/education_stage_operation.dart';
 import '../../core/database/sqlflite/groups_operation.dart';
 import '../../core/resources/route_manager.dart';
-import '../../view/education_stages/widget/search/custom_list_search_education_stage_screen.dart';
 
 class GroupsScreenController {
   late StreamController<List<GroupInfoModel>> controllerListItemGroupModel;
@@ -18,6 +13,7 @@ class GroupsScreenController {
   late Stream<List<GroupInfoModel>> outPutDataListItemGroupModel;
   List<GroupInfoModel> listGroupInfo = [];
   BuildContext context;
+  bool isSearchNow = false;
 
   GroupsScreenController(this.context) {
     start();
@@ -76,8 +72,11 @@ class GroupsScreenController {
                 GroupsOperation groupOperation = GroupsOperation();
                 bool deleted = await groupOperation.startDelete(groupInfoModel);
                 if (deleted) {
-                  Navigator.of(context).pop(false);
-
+                  if (isSearchNow == true) {
+                    Navigator.of(context).pop(true);
+                  } else {
+                    Navigator.of(context).pop(false);
+                  }
                   onRefresh();
                 }
               },
@@ -90,16 +89,26 @@ class GroupsScreenController {
         ],
       ),
     );
+    if (confirmDelete == true) {
+      Navigator.of(context).pop();
+    }
   }
 
   void editGroupInfo(GroupInfoModel groupInfoModel) {
     Navigator.of(context).pushNamed(RoutesName.kAddGroupScreen, arguments: {
       ConstValue.kStatus: ConstValue.kEditThisGroup,
       ConstValue.kGroupInfoModel: groupInfoModel,
-    }).then((value) => getAllData());
+    }).then((value) {
+      getAllData();
+      if (isSearchNow == true) {
+        Navigator.of(context).pop();
+        isSearchNow = false;
+      }
+    });
   }
 
   void onPressedSearch() {
+    isSearchNow = true;
     showSearch(
         context: context,
         delegate: CustomSearchDelegated(
@@ -110,14 +119,16 @@ class GroupsScreenController {
                 : CustomListSearchGroupScreen(
                     getSearchItemsgroup:
                         groupsOperation.getSearchWord(groupName: query),
-                    deleteFun: deleteGroupInfo,
                     editFun: (groupInfoModel) {
                       editGroupInfo(
                         groupInfoModel,
                       );
                     },
+                    deleteFun: (GroupInfoModel groupInfoModel) {
+                      deleteGroupInfo(groupInfoModel);
+                    },
                   );
           },
-        ));
+        )).then((value) => isSearchNow = false);
   }
 }
